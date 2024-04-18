685dc4819da706fa8a8edf74743baaafd77bca00
Rajeev <rkcoola792@gmail.com>
2024-04-18 13:01:52 +0530

    testimonial fixed

--- src/components/homepage/Section7.jsx
+++ src/components/homepage/Section7.jsx
@@ -9,7 +9,7 @@ import { motion } from 'framer-motion';
 const Section7 = () => {
   return (
     <div className="section7   my-12  px-4 sm:px-12 lg:px-12 2xl:px-0 flex flex-col justify-center items-center gap-6">
-      <div className="top-heading sticky md:static top-[85px]  -mt-2 z-40 bg-white py-6">
+      <div className="top-heading  top-[85px]  -mt-2 z-40 bg-white py-6">
         <Heading
           heading="testimonials"
           subheading="Hear what our client say"
